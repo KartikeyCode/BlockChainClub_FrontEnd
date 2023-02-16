16f515724c4d75d77df731f06f913f6015cc4a81
Kartikey <codekartikey@gmail.com>
2023-02-16 17:15:35 +0530

    first commit

---
+++ uptal/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <>
+    <div>
+      <h1 className='Logo'></h1>
+    </div>
+    </>
+  );
+}
+
+export default App;
