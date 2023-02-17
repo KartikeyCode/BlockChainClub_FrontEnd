54d4022f8a0bc221d6f1aa88023478181c9c8889
Kartikey <codekartikey@gmail.com>
2023-02-17 19:27:58 +0530

    complete first page

--- uptal/src/App.js
+++ uptal/src/App.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import './App.css';
+import MainImage from "./pictures/main.png"
+import ArrayElements from './ArrayElement';
 import Container from './Leftcontainer';
 function App() {
   return (
@@ -10,10 +12,18 @@ function App() {
       <div className='TopContainer'>
         <Container/>
         <div className='TopContainer-Right'>
+          <div>
           <h1 className='TCL-Text Welcome'>Welcome John!</h1>
           <h2 className='Sub'>How to get jobs with top U.S. companies</h2>
-          </div> 
-      </div>
+          </div>
+          <div className='TCR-Array'>
+            <ArrayElements/>
+          </div>
+          <div>
+           <img className="Main" src={MainImage}></img>
+          </div>
+          </div>
+      </div> 
       </div>
       </div>
     </>
---
+++ uptal/src/ArrayElement.js
@@ -0,0 +1,24 @@
+import React from "react";
+function ArrayElements(){
+    return(
+        <>
+        <div className='ArrayElement'>
+              <div className='ArrayCircle'>1</div>
+              <h3 className='ArrayText'>Take Tests</h3>
+            </div>
+            <div className='ArrayElement'>
+              <div className='ArrayCircle'>2</div>
+              <h3 className='ArrayText'>Pass Live <br/>Coding Challenge</h3>
+            </div>
+            <div className='ArrayElement'>
+              <div className='ArrayCircle'>3</div>
+              <h3 className='ArrayText'>Finalize your resume</h3>
+            </div>
+            <div className='ArrayElement'>
+              <div className='ArrayCircle'>4</div>
+              <h3 className='ArrayText'>Receive job offers</h3>
+            </div>
+        </>
+    )
+}
+export default ArrayElements;
